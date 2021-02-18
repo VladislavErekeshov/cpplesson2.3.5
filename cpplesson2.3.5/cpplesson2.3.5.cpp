f378093d91f6863429343f928d3953b30ab338f2
VladislavErekeshov <tommysyching@gmail.com>
2021-02-18 06:46:54 +0300

    Добавьте файлы проекта.

---
+++ cpplesson2.3.5/cpplesson2.3.5.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    int m, n, am, bm, cm, an, bn, cn;
+    cin >> m >> n;
+    am = m / 100 % 10;
+    an = n / 100 % 10;
+    bm = m / 10 % 10;
+    bn = n / 10 % 10;
+    cm = m  % 10;
+    cn = n  % 10;
+    cout << am + an << " " << bm + bn << " " << cm + cn;
+    return 0;
+}
